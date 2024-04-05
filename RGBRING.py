df12506f7816edd9f7ad264fa8fb47cf3e98a254
Ahmed MJADI <92577300+mjadiahmed@users.noreply.github.com>
2024-04-05 20:08:45 +0000

    Update RGBRING.py

--- RGBRING.py
+++ RGBRING.py
@@ -2,9 +2,9 @@
 import time
 import board
 import neopixel
-pixel_pin = board.D18
-num_pixels = 12
-ORDER = neopixel.GRB
+pixel_pin = board.D18  #GPIO PIN
+num_pixels = 12  #how many LEDs
+ORDER = neopixel.GRB  
 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, brightness=1, auto_write=False, pixel_order=ORDER
